948ff53eedaf82825c92defcf01a62be1ecc3f0c
李志诚 <1404204769@qq.com>
2022-07-22 18:48:27 +0800

    完成了角色选择界面1.0

---
+++ assets/Script/Scene/RoleChoice/HeroChoice.ts
@@ -0,0 +1,33 @@
+import Hero from "./Hero";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    @property
+    HeroID: string = "error";
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        this.node.on(cc.Node.EventType.MOUSE_UP, this.ShowHeroDetail, this);
+    }
+
+    // update (dt) {}
+    ShowHeroDetail (event) {
+        console.log("选择的英雄ID为"+this.HeroID);
+        // 得到全局控制脚本
+        let RoleChoiceScene = cc.find("Canvas/Main Layout/Data").getComponent("RoleChoiceScene");
+        RoleChoiceScene.LoadHeroData(parseInt(this.HeroID))
+    }
+
+    onDestroy(){
+        this.node.off(cc.Node.EventType.MOUSE_UP, this.ShowHeroDetail, this);
+    }
+
+    init(ID:number){
+        this.HeroID = String(ID)
+    }
+}
