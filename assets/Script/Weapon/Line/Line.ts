16564456f76fe6382fb3321376a459a02776ba57
liwl <lwlong0922@foxmail.com>
2022-07-24 19:29:10 +0800

    Signed-off-by: liwl <“lwlong0922@foxmail.com>

--- assets/Script/Weapon/Line/Line.ts
+++ assets/Script/Weapon/Line/Line.ts
@@ -4,7 +4,7 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class Line extends Weapon {
 
-    start() {        
+    start() {
         let boxCollider = this.addComponent(cc.BoxCollider)
         boxCollider.size = this.data.size
     }
@@ -12,8 +12,11 @@ export default class Line extends Weapon {
     move() {
         let targetPos = this.getTarget().mulSelf(cc.winSize.width * 2)
         let time = targetPos.len() / this.data.speed
+
+        let ang = targetPos.angle(cc.v2(1, 0)) / (Math.PI * 360)
+
         this.moveTween = cc.tween(this.node)
-            .by(time, {position: cc.v3(targetPos.x, targetPos.y)})
+            .by(time, { position: cc.v3(targetPos.x, targetPos.y) })
             .removeSelf()
             .start()
     }
